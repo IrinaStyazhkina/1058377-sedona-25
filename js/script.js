355e44c00b7efd8ccff1797218be3624a68246f5
IrinaStyazhkina <irastyazhkina@gmail.com>
2019-06-28 23:55:22 +0300

    Final version
    Final version

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var link = document.querySelector(".search-link");
-		var popup = document.querySelector(".search-form");
-		link.addEventListener("click", function(evt){
-		evt.preventDefault();
-		popup.classList.toggle("search-form-close");
-		});
+var popup = document.querySelector(".search-form");
+link.addEventListener("click", function(evt){
+	evt.preventDefault();
+	popup.classList.toggle("search-form-close");
+});
